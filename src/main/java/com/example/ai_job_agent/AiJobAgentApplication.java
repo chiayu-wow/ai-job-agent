7586463d1ac2734f47b902442655cce13870226a
CHIAYU WANG <chiayuwang@CHIAYUdeMacBook-Air-6.local>
2026-05-26 15:48:09 -0500

    feat: initial Spring Boot setup

---
+++ src/main/java/com/example/ai_job_agent/AiJobAgentApplication.java
@@ -0,0 +1,13 @@
+package com.example.ai_job_agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiJobAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiJobAgentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ai_job_agent/AiJobAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ai_job_agent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiJobAgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
